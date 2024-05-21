1ae65d5e2d281929b99247a98133592ed2e807b4
babywizzies <waheguru1111@protonmail.com>
2024-05-21 11:38:40 -0400

    Font Integration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       fontFamily: {
-        custom: ['Mek', 'sans-serif'],
+        meksans: ['MEKSANS', 'sans-serif'],
       },
     },
   },
