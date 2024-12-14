057c4f40d2738dec94d4dccb993aeafb6f7004aa
Pleasures <iammpjdcp@gmail.com>
2024-12-14 10:57:31 +0800

    Edit port to 3011

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const fs = require('fs/promises');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3011;
 
 // Middleware
 app.use(cors()); // Enable CORS for Next.js frontend
